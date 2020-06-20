cf1a3fbee333c8541833ec6398e4b8cfa53fd8be
pennington78750 <53716713+pennington78750@users.noreply.github.com>
2020-06-20 12:38:44 +0530

    Update and rename app.js to appp.js

--- js/appp.js
+++ js/appp.js
@@ -12,4 +12,8 @@ window.addEventListener("click", (e) => {
         navItem.classList.remove("active");
         nav.classList.remove("active");
     }
-});
+});const selectElement = (element) => document.querySelector(element);
+selectElement(".hamburger").addEventListener("click", () => {
+    selectElement(".hamburger").classList.toggle("active");
+    selectElement(".nav-list").classList.toggle("active");
+
